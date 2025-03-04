7e526718b7cc7c67753df09dd5bf8a1885215f1f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2025-03-04 18:09:15 -0300

    Fundamentos básicos de Python. Curso fundação bradesco"

---
+++ fundamentos/exibir_variavel.py
@@ -0,0 +1,10 @@
+codigo = 105
+salario = 1500.00
+nome = "Jose Santana"
+ativo = True
+
+
+print("Código: %d" % codigo)
+print("Salário: %.2f" % salario)
+print("Nome: %s" % nome)
+print("Ativo: %r" % ativo)
---
+++ fundamentos/tipo_variavel.py
@@ -0,0 +1,22 @@
+codigo = 10
+salario = 1500.00
+nome = "Jose"
+situacao = True
+
+tipo = type(salario)
+
+print(salario)
+print(tipo)
+
+print("Codigo:", codigo, "Salario:", salario, "Nome:", nome, "Situacao:", situacao)
+
+print(
+    "Codigo: "
+    + str(codigo)
+    + " Salario: "
+    + str(salario)
+    + " Nome: "
+    + nome
+    + " Situacao: "
+    + str(situacao)
+)
