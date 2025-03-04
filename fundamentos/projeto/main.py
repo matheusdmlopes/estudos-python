6231a835c1e5b86be01ca1b7106bdd9c6cb1313e
matheusdmlopes <matheusdemarcolopes@gmail.com>
2025-03-04 19:48:46 -0300

    Exercício/Projeto do curso finalizado

--- fundamentos/projeto/main.py
+++ fundamentos/projeto/main.py
@@ -6,8 +6,12 @@ print("Testando o projeto.")
 
 from cliente import Cliente
 
-cliente1 = Cliente("João", "1234-5678")
+from conta import Conta
 
-print(cliente1)
-print(cliente1.nome)
-print(cliente1.telefone)
+cliente1 = Cliente("João", "1234-5678")
+conta = Conta("123-4", cliente1.nome, 120.0, 1000.0)
+
+print("Nome do cliente: ", conta.titular)
+print("Saldo do cliente: ", conta.saldo)
+print("Limite do cliente: ", conta.limite)
+print("Número da conta: ", conta.numero)
