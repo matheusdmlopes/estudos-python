7dc7bfcbbde933971fd8b266364f6af0b6069e99
matheusdmlopes <matheusdemarcolopes@gmail.com>
2025-03-04 19:44:03 -0300

    Inicio do projeto de estudos

---
+++ fundamentos/projeto/main.py
@@ -0,0 +1,13 @@
+class Main:
+    pass
+
+
+print("Testando o projeto.")
+
+from cliente import Cliente
+
+cliente1 = Cliente("João", "1234-5678")
+
+print(cliente1)
+print(cliente1.nome)
+print(cliente1.telefone)
