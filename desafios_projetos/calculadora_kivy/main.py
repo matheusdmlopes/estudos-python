4db202983bce22ec85636babee9bf2a24e4d31a8
matheusdmlopes <matheusdemarcolopes@gmail.com>
2025-03-05 19:56:20 -0300

    Calculadora em kivy

--- desafios_projetos/calculadora_kivy/main.py
+++ desafios_projetos/calculadora_kivy/main.py
@@ -11,7 +11,7 @@ class MainApp(App):
         self.last_button = None
         main_layout = BoxLayout(orientation="vertical")
         self.solution = TextInput(
-            multi_line=False, readonly=True, halign="right", font_size=55
+            multiline=False, readonly=True, halign="right", font_size=55
         )
         main_layout.add_widget(self.solution)
         buttons = [
@@ -33,3 +33,30 @@ class MainApp(App):
         main_layout.add_widget(equals_button)
 
         return main_layout
+
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+
+        if button_text == "C":
+            self.solution.text = ""
+        else:
+            if current and (self.last_was_operator and button_text in self.operators):
+                return
+            elif current == "" and button_text in self.operators:
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
+
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+
+
+if __name__ == "__main__":
+    MainApp().run()
