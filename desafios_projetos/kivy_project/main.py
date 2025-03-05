0f230d9fccd343c4321025615e48dd85c153653f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2025-03-05 19:14:43 -0300

    Testes do kivy com botões em interface gráfica simples

--- desafios_projetos/kivy_project/main.py
+++ desafios_projetos/kivy_project/main.py
@@ -11,18 +11,19 @@ blue = [0, 0, 1, 1]
 purple = [1, 0, 1, 1]
 
 
-class HBoxLayoutExample(App):
+class MainApp(App):
     def build(self):
-        layout = BoxLayout(padding=10)
-        colors = [red, green, blue, purple]
+        button = Button(
+            text="Hello from Kivy",
+            size_hint=(0.5, 0.5),
+            pos_hint={"center_x": 0.5, "center_y": 0.5},
+        )
+        button.bind(on_press=self.on_press_button)
+        return button
 
-        for i in range(5):
-            btn = Button(
-                text="Button #%s" % (i + 1), background_color=random.choice(colors)
-            )
-            layout.add_widget(btn)
-        return layout
+    def on_press_button(self, instance):
+        print("Button pressed")
 
 
 if __name__ == "__main__":
-    HBoxLayoutExample().run()
+    MainApp().run()
